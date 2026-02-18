23d29747a4d34393ae01c7eaec89f8d80604d10d
Nishmith12 <nishmith101@gmail.com>
2026-02-18 23:25:41 +0530

    Resolved

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+
+console.log("API Base URL:", apiUrl);
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: apiUrl,
 });
 
 export default api;
