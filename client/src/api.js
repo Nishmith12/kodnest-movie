29693c70d5e2c7fc432ed2c3a2a7022da3a31e85
Nishmith12 <nishmith101@gmail.com>
2026-02-18 23:34:49 +0530

    Final

--- client/src/api.js
+++ client/src/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 let apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
+// Remove trailing slash if present to avoid double slashes (e.g., .app//api)
+apiUrl = apiUrl.replace(/\/+$/, '');
+
 if (!apiUrl.endsWith('/api')) {
     apiUrl += '/api';
 }
